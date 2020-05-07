d245978274186e4e1b4d6136f11341911959e6d7
Tony Pulickal <ttp@hashicorp.com>
2020-05-07 10:36:07 -0400

    remove random string resource

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "ttp-demo-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "ttp-demo-eks"
 }
 
 module "vpc" {
